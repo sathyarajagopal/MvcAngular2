81d4b2c7eee34ba2478fd7ecd09353d8f445df68
sathyarajagopal <smartaxgenius@gmail.com>
2017-07-03 23:01:12 -0400

    Working solution commit.

---
+++ MvcAngular2/src/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+
+namespace MvcAngular2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcAngular2/src/Controllers/PartialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+
+namespace MvcAngular2.Controllers
+{
+    public class PartialController : Controller
+    {
+        public IActionResult Message() => PartialView();
+        
+        public IActionResult Numbers() => PartialView();
+    }
+}
